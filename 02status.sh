f1d6cc81ff1f79becf1c582e01c75d21cd2c7385
e-niale <lielaine00@gmail.com>
2025-01-07 18:34:49 -0800

    new 1/7

---
+++ 02status.sh
@@ -0,0 +1,10 @@
+#reports date
+date
+#reports uniz type
+uname -a
+#reports environmental variables
+printenv
+#reports version of python
+python3 --version
+#reports directories and files in Code directory
+ls -R ~/Code
