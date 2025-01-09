ecf83cfdb124368b82ad847066b95c86156d5ef8
e-niale <lielaine00@gmail.com>
2025-01-08 22:53:26 -0800

    edit 1

--- 04spellingbee.sh
+++ 04spellingbee.sh
@@ -1,1 +1,1 @@
-gunzip -c ~/Code/MCB185/data/dictionary.gz | grep -v "[^zoniarc]" | grep ".*r.*" | grep -E ".{4,}"
+gunzip -c ~/Code/MCB185/data/dictionary.gz | grep -v "[^zoniarc]" | grep "r" | grep -E ".{4,}"
